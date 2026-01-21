81a19c929c9fd0de2c1fef8c326ebf59d75926c6
Louis Choiniere <louischoiniere@outlook.com>
2026-01-21 00:09:58 -0500

    Update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { config } from "@fortawesome/fontawesome-svg-core";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/favicon.png";
 	import { page } from "$app/state";
 
 	// Prevent FontAwesome from dynamically adding its CSS since we did it manually
